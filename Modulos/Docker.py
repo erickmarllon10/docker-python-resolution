6f98cdefbcc3ec84e2006ef133486e08b98bde7a
Erick Marllon <erickmarllon10@gmail.com>
2018-04-20 12:32:37 -0300

    First commit

---
+++ Modulos/Docker.py
@@ -0,0 +1,15 @@
+class Docker:
+    def __init__(self):
+        self.image = "webservercloud"
+
+    def criarContainer(self,nome):
+        return "docker run -tdi --name %s --hostname %s %s /bin/bash"%(nome,nome,self.image)
+
+    def pegarIPContainer(self,nome):
+        return "docker inspect %s"%nome
+
+    def acessarContainer(self,nome,comando):
+        return "docker exec %s /bin/bash -c '%s'"%(nome,comando)
+
+    def removerContainer(self,nome):
+        return "docker stop %s && docker rm %s"%(nome,nome)
---
+++ Readme.md
@@ -0,0 +1,10 @@
+## Creating, removing and acessing Docker containers
+
+#### task2 solution for class 9 of Python Fundamentals Course - 4Linux
+
+#### Using the packages below:
+
+* **Python version** = _2.7_
+* **Models ** = _Pymongo_
+* **Databases ** = _MongoDB_
+* **Modules** = _Piramiko_ 
